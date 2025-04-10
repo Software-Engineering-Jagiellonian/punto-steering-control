ff08b36176c3aa0748a69716ca76d5a857c44792
eevee71 <oliwka.pazdro@gmail.com>
2025-04-10 23:38:31 +0200

    cmake configuration, googletest

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/oliwi/projects/punto-steering-control1/punto-steering-control/punto-steering-control
+# Build directory: C:/Users/oliwi/projects/punto-steering-control1/punto-steering-control/punto-steering-control/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("interface")
+subdirs("io")
+subdirs("tests")
+subdirs("_deps/googletest-build")
